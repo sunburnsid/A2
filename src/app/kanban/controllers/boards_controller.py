bb3d2d93f31542b3848a93dbed65fdedf35637af
sunburnsid <sb846@cornell.edu>
2018-04-30 18:56:54 -0400

    POST board start

--- src/app/kanban/controllers/boards_controller.py
+++ src/app/kanban/controllers/boards_controller.py
@@ -1,6 +1,14 @@
 from app.constants import *
 from . import *
+from flask import jsonify
 
-@kanban.route('/boards', methods=['GET', 'POST', 'DELETE'])
-def boards_crud():
-  return jsonify("Hello World!")
+@kanban.route('/boards', methods=['POST'])
+def boards_post():
+  board = create_board(request.args.get('board_title'))
+  return jsonify(board)
+
+@kanban.route('/boards', methods=['GET'])
+def boards_get():
+
+@kanban.route('/boards', methods=['DELETE'])
+def boards_delete():
--- src/app/kanban/dao/boards_dao.py
+++ src/app/kanban/dao/boards_dao.py
@@ -10,3 +10,18 @@ def board_by_id(board_id):
   Get board by ID
   """
   return Board.query.filter_by(id=board_id).first()
+
+
+def create_board(board_title):
+  """
+  Create a board with board_title as title
+  """
+  board = Board(title=board_title) # create instance of the board
+  db.session.add(board) # add it to the current session
+  try:
+    db.session.commit() # push this session to the DB
+    return board
+  except Exception as e:
+    db.session.rollback() # rollback the session if there was an exception
+    return e
+  return board
