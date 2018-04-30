191dd94d4519c9cefa08cf5d6df73516b17f4c46
sunburn <siddantb@gmail.com>
2018-04-30 19:18:28 -0400

    traces for controllers

--- src/app/kanban/controllers/boards_controller.py
+++ src/app/kanban/controllers/boards_controller.py
@@ -4,13 +4,16 @@ from flask import jsonify
 
 @kanban.route('/boards', methods=['POST'])
 def boards_post():
-  board = create_board(request.args.get('board_title'))
-  return jsonify(board)
+  board = boards_dao.create_board(request.args.get('board_title'))
+  print("AM I HAPPENING")
+  return jsonify({'success':True})
 
 @kanban.route('/boards', methods=['GET'])
 def boards_get():
+  print("OR IS IT ME")
   pass
 
 @kanban.route('/boards', methods=['DELETE'])
 def boards_delete():
+  print("OR IS IT ME")
   pass
