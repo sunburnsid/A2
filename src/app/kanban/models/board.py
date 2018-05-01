f54dd06a7c3500fdb470c3218a76e43b1d1fa98b
sunburnsid <sb846@cornell.edu>
2018-04-30 21:31:28 -0400

    Movement

--- src/app/kanban/models/board.py
+++ src/app/kanban/models/board.py
@@ -11,3 +11,19 @@ class Board(Base):
     Constructor
     """
     self.title = kwargs.get('title', None)
+
+
+class Element(Base):
+  __tablename__ = 'elements'
+  id             = db.Column(db.Integer, primary_key=True)
+  board_id       = db.Column(db.Integer, db.ForeignKey('board.id'))
+  category       = db.Column(db.String(256), nullable=False)
+  description    = db.Column(db.String(512), nullable=False)
+
+  def __init__(self, **kwargs):
+    """
+    Constructor
+    """
+    self.category = kwargs.get('category', None)
+    self.board_id = kwargs.get('board_id', None)
+    self.description = kwargs.get('description', None)
