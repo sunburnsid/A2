7299f027052f5ad12e39d2416c25db14ceadb14c
sunburn <siddantb@gmail.com>
2018-04-30 21:30:59 -0400

    lol

--- src/app/kanban/models/board.py
+++ src/app/kanban/models/board.py
@@ -4,7 +4,7 @@ class Board(Base):
   __tablename__ = 'boards'
   id             = db.Column(db.Integer, primary_key=True)
   title          = db.Column(db.String(256), unique=True, nullable=False)
-  board_elements = db.relationship('BoardElement')
+  #board_elements = db.relationship('Element')
 
   def __init__(self, **kwargs):
     """
